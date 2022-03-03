3c9117a94ab494dbef6d1f3fcd1f7d3167495169
thoranna <thoranna18@ru.is>
2022-03-03 10:14:43 +0100

    board ready

--- aigame.py
+++ aigame.py
@@ -1,37 +1,41 @@
 import pygame
 from pygame.locals import * 
 
-from OpenGL.GL import *
-from OpenGL.GLU import *
-
-WINDOWWIDTH = 800
-WINDOWHEIGHT = 600
+WINDOWWIDTH = 675
+WINDOWHEIGHT = 675
+BLOCKCOLOR = (250, 250, 250)
+BOARDCOLOR = (210,180,140)
+BLACK = (0, 0, 0)
 
 class Game: 
     def __init__(self):
-        self.clock = pygame.time.Clock()
         pygame.init()
         self.game_window = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-        self.game_window.fill((0, 0, 0))
-        pygame.display.set_caption("AI Game")
-        self.clock.tick()
-        # pygame.display.flip()
+        pygame.display.set_caption("Taiji")
 
     def display(self):
         pygame.display.flip()
         pygame.display.update()
 
+    def draw_grid(self):
+        self.block_size = 75
+        for x in range(0, WINDOWWIDTH, self.block_size):
+            for y in range(0, WINDOWHEIGHT, self.block_size):
+                rect = pygame.Rect(x, y, self.block_size, self.block_size)
+                pygame.draw.rect(self.game_window, BLOCKCOLOR, rect, 1)
 
 if __name__ == "__main__":
     game = Game()
     game_active = True
-    # game.display()
     while game_active:
+        game.draw_grid()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_active = False
-        
-        game.game_window.fill((0, 0, 0))
+                gameActive = False
+
+        game.game_window.fill((BOARDCOLOR))        
+        game.draw_grid()
         pygame.display.flip()
-        pygame.display.update()
-        # game.display()
+
+pygame.quit()
+quit()
